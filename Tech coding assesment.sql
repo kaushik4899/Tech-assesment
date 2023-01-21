bc5d7e791a840a2f0b9769ae59b053d1995e74ae
Kaushik k. Kajale <115804611+kaushik4899@users.noreply.github.com>
2023-01-21 18:26:41 +0530

    Add files via upload

---
+++ Tech coding assesment.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE EDYODA;
+USE EDYODA;
+CREATE TABLE IF NOT EXISTS countries
+(
+country_id varchar(2),
+country_name varchar(40)
+CHECK(country_name IN('Italy','India','China')),
+region_id decimal(10,0)
+);
+
